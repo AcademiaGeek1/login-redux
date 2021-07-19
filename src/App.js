74410c43a8545575e6da9d28cf71e53c51d5a4c0
Henry <luisgasthof@hotmail.com>
2021-07-19 09:25:52 -0500

    formulario basico e impresion por consola

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Button, Form } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+import { useForm } from './hooks/useForm'
 
-function App() {
+const App = () => {
+  const [formValues, handleInputChange] = useForm({
+    email: '',
+    password: ''
+  })
+
+  const { email, password } = formValues
+
+  const handleLogin = (e) => {
+    e.preventDefault()
+    console.log(email, password)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Form onSubmit={handleLogin}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Correo Electrónico</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            name="email"
+            value={email}
+            onChange={handleInputChange}
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control 
+          type="password" 
+          placeholder="Password" 
+          name="password"
+          value={password}
+          onChange={handleInputChange}
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
